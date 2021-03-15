1aaef8be454fcafb77bb4ef08d115287fc447e5d
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2021-03-16 00:18:51 +0900

    C언어 <ctype.h>의 iscntrl(제어 문자인지 확인해준다.) 함수 연습!(2)

--- C_iscntrl_2.c
+++ C_iscntrl_2.c
@@ -6,7 +6,7 @@ int main(void)
 	char a = '1'; // 초기화
 
 	printf("만약, 제어 문자면 0이 아닌 수가 나올 거예요.\n");
-	printf("%d", iscntrl(a)); // 출력하기
+	printf("%d", iscntrl(a)); // 제어 문자인지 아닌지 확인하여 출력하기
 
 	return 0;
 }
